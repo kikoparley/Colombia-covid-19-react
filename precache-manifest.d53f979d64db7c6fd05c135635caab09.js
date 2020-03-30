e6f741120459fb5a21d774978639731601985ef1
alexander fernandnez <villarrealalex@outlook.com>
2020-03-29 20:32:46 -0500

    Updates

---
+++ precache-manifest.d53f979d64db7c6fd05c135635caab09.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84105aa2ead70200ce360161a8c9ef2b",
+    "url": "/Colombia-covid-19-react/index.html"
+  },
+  {
+    "revision": "c83466046b1c98b5b459",
+    "url": "/Colombia-covid-19-react/static/css/main.5db5077d.chunk.css"
+  },
+  {
+    "revision": "dca9502115f3134fb588",
+    "url": "/Colombia-covid-19-react/static/js/2.fbfe2a18.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/Colombia-covid-19-react/static/js/2.fbfe2a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83466046b1c98b5b459",
+    "url": "/Colombia-covid-19-react/static/js/main.d9a46081.chunk.js"
+  },
+  {
+    "revision": "edca6aa8cfa8159154d9",
+    "url": "/Colombia-covid-19-react/static/js/runtime-main.33b28a27.js"
+  },
+  {
+    "revision": "6860373c72155a3c0d56fc1fd81923b1",
+    "url": "/Colombia-covid-19-react/static/media/blueHeader.6860373c.6860373c.svg"
+  },
+  {
+    "revision": "efa57e1f2a89b4d1c37fd54d950eb971",
+    "url": "/Colombia-covid-19-react/static/media/logo.efa57e1f.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Colombia-covid-19-react/static/media/stars.1d17360a.svg"
+  }
+]);
